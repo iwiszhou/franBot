56d4d8dc7898d822828b6105e2cdd3bb4c40cc23
Frank Kitholt <frankkitholt@Franks-Big-Mac.local>
2024-03-22 20:10:35 +0800

    final deploy k8s

--- ChatGPT_HKBU.py
+++ ChatGPT_HKBU.py
@@ -23,7 +23,7 @@ class HKBU_ChatGPT():
         url = (self.config['CHATGPT']['BASICURL']) + "/deployments/" + (
             self.config['CHATGPT']['MODELNAME']) + "/chat/completions/?api-version=" + (self.config['CHATGPT']['APIVERSION'])
         headers = {'Content-Type': 'application/json',
-                   'api-key': self.config['CHATGPT']['ACCESS_TOK']}
+                   'api-key': self.config['CHATGPT']['ACCESS_TOKEN']}
 
         payload = {'messages': conversation}
         response = requests.post(url, json=payload, headers=headers)
