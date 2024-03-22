da9ea8a4de994da8dba202695528149328d90882
Frank Kitholt <frankkitholt@Franks-Big-Mac.local>
2024-03-22 19:45:26 +0800

    edit gpt token

--- ChatGPT_HKBU.py
+++ ChatGPT_HKBU.py
@@ -23,7 +23,7 @@ class HKBU_ChatGPT():
         url = (self.config['CHATGPT']['BASICURL']) + "/deployments/" + (
             self.config['CHATGPT']['MODELNAME']) + "/chat/completions/?api-version=" + (self.config['CHATGPT']['APIVERSION'])
         headers = {'Content-Type': 'application/json',
-                   'api-key': self.config['CHATGPT']['ACCESS_TOKEN']}
+                   'api-key': self.config['CHATGPT']['ACCESS_TOK']}
 
         payload = {'messages': conversation}
         response = requests.post(url, json=payload, headers=headers)
